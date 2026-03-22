36d815ddcbb4edc58501f32593a9edcb30802b89
Carlos Aragonés <c.aragones@gmail.com>
2026-03-22 17:18:16 +0100

    fullscreen example: Test both fullscreen and desktop fullscreen

--- tests/fullscreen.c
+++ tests/fullscreen.c
@@ -4,42 +4,75 @@
 
 //-------------------------------------
 #define kUnused(var)    (void) var
+#define TEST_TAG        "fullscreen"
 
 //-------------------------------------
-#define WIDTH      960
+#define WIDTH      800
 #define HEIGHT     640
 static unsigned int g_buffer[WIDTH * HEIGHT];
 static bool         g_active = true;
 
+void
+draw_noise(struct mfb_window *window) {
+    static int seed = 0xbeef;
+    int i;
+    int noise, carry;
+
+    for (i = 0; i < WIDTH * HEIGHT; ++i) {
+        noise = seed;
+        noise >>= 3;
+        noise ^= seed;
+        carry = noise & 1;
+        noise >>= 1;
+        seed >>= 1;
+        seed |= (carry << 30);
+        noise &= 0xFF;
+        g_buffer[i] = MFB_RGB(noise, noise, noise);
+    }
+}
+
 //-------------------------------------
 int
 main() {
-    int noise, carry, seed = 0xbeef;
+    mfb_update_state state;
 
+    MFB_LOGI(TEST_TAG, "Full Screen Window");
     struct mfb_window *window = mfb_open_ex("full screen auto", WIDTH, HEIGHT, MFB_WF_FULLSCREEN);
     if (!window) {
+        MFB_LOGE(TEST_TAG, "Cannot create window");
         return 0;
     }
 
     mfb_set_viewport_best_fit(window, WIDTH, HEIGHT);
 
     do {
-        int              i;
-        mfb_update_state state;
-
         if (g_active) {
-            for (i = 0; i < WIDTH * HEIGHT; ++i) {
-                noise = seed;
-                noise >>= 3;
-                noise ^= seed;
-                carry = noise & 1;
-                noise >>= 1;
-                seed >>= 1;
-                seed |= (carry << 30);
-                noise &= 0xFF;
-                g_buffer[i] = MFB_RGB(noise, noise, noise);
-            }
+            draw_noise(window);
+            state = mfb_update(window, g_buffer);
+        }
+        else {
+            state = mfb_update_events(window);
+        }
 
+        if (state != MFB_STATE_OK) {
+            window = NULL;
+            break;
+        }
+    } while (mfb_wait_sync(window));
+
+    //---------------------------------
+    MFB_LOGI(TEST_TAG, "Desktop Full Screen Window");
+    window = mfb_open_ex("full screen auto", WIDTH, HEIGHT, MFB_WF_FULLSCREEN_DESKTOP);
+    if (!window) {
+        MFB_LOGE(TEST_TAG, "Cannot create window");
+        return 0;
+    }
+
+    mfb_set_viewport_best_fit(window, WIDTH, HEIGHT);
+
+    do {
+        if (g_active) {
+            draw_noise(window);
             state = mfb_update(window, g_buffer);
         }
         else {
